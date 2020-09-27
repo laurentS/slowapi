b82536a669365970251e97b12d48ec0b7f6f8162
Laurent Savaete <laurent@where.tf>
2020-09-28 00:42:46 +0100

    Fix black and mypy errors

--- tests/test_fastapi_extension.py
+++ tests/test_fastapi_extension.py
@@ -1,5 +1,5 @@
-import hiro
-import pytest
+import hiro  # type: ignore
+import pytest  # type: ignore
 from starlette.requests import Request
 from starlette.responses import PlainTextResponse
 from starlette.testclient import TestClient
@@ -43,29 +43,37 @@ class TestDecorators(TestSlowapi):
 
             assert cli.get("/t1").status_code == 429
             assert (
-                    cli.get("/t1", headers={"X_FORWARDED_FOR": "127.0.0.3"}).status_code
-                    == 429
+                cli.get("/t1", headers={"X_FORWARDED_FOR": "127.0.0.3"}).status_code
+                == 429
             )
 
     def test_endpoint_missing_request_param(self):
         app, limiter = self.build_fastapi_app(key_func=get_ipaddr)
 
         with pytest.raises(Exception) as exc_info:
+
             @app.get("/t3")
             @limiter.limit("5/minute")
             async def t3():
                 return PlainTextResponse("test")
-        assert exc_info.match(r"""^No "request" or "websocket" argument on function .*""")
+
+        assert exc_info.match(
+            r"""^No "request" or "websocket" argument on function .*"""
+        )
 
     def test_endpoint_missing_request_param_sync(self):
         app, limiter = self.build_fastapi_app(key_func=get_ipaddr)
 
         with pytest.raises(Exception) as exc_info:
+
             @app.get("/t3_sync")
             @limiter.limit("5/minute")
             def t3():
                 return PlainTextResponse("test")
-        assert exc_info.match(r"""^No "request" or "websocket" argument on function .*""")
+
+        assert exc_info.match(
+            r"""^No "request" or "websocket" argument on function .*"""
+        )
 
     def test_endpoint_request_param_invalid(self):
         app, limiter = self.build_fastapi_app(key_func=get_ipaddr)
@@ -78,7 +86,9 @@ class TestDecorators(TestSlowapi):
         with pytest.raises(Exception) as exc_info:
             client = TestClient(app)
             client.get("/t4")
-        assert exc_info.match(r"""parameter `request` must be an instance of starlette.requests.Request""")
+        assert exc_info.match(
+            r"""parameter `request` must be an instance of starlette.requests.Request"""
+        )
 
     def test_endpoint_request_param_invalid_sync(self):
         app, limiter = self.build_fastapi_app(key_func=get_ipaddr)
@@ -91,4 +101,6 @@ class TestDecorators(TestSlowapi):
         with pytest.raises(Exception) as exc_info:
             client = TestClient(app)
             client.get("/t5")
-        assert exc_info.match(r"""parameter `request` must be an instance of starlette.requests.Request""")
+        assert exc_info.match(
+            r"""parameter `request` must be an instance of starlette.requests.Request"""
+        )
